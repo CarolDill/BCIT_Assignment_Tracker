b3c6ab31b2c1e94bbd3986ca45963ffecec41365
CarolDill <carol37_dill@msn.com>
2023-05-13 22:39:27 -0700

    1 done
    1. The Create button should be DISABLED when no assignment has been entered into the input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,13 +7,21 @@ import { useState } from "react";
 export function Header() {
 
   const [disableCreate, setDisableCreate] = useState(true);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if(!/\S/.test(e.target.value)){
+      setDisableCreate(true);
+    } else {
+      setDisableCreate(false);
+    }
+  }
   
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
+        <input placeholder="Add a new assignment" type="text" onChange={handleChange}/>
         <button disabled={disableCreate}>
           Create <AiOutlinePlusCircle size={20} />
         </button>
